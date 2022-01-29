c8d9cd96303a8e6b5bc62ba3fbc135c2d9f97f3c
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-01-29 10:04:04 +0100

    ClientListener verbessert

--- Game of Colours/src/de/gyko/gameofcolors/net/ClientListener.java
+++ Game of Colours/src/de/gyko/gameofcolors/net/ClientListener.java
@@ -1,5 +1,12 @@
 package de.gyko.gameofcolors.net;
 
-public interface ClientListener {
+import de.gyko.gameofcolors.net.event.*;
+import de.gyko.netLib.PacketSendRequest;
 
+import java.util.ArrayList;
+
+public interface ClientListener {
+    ArrayList<PacketSendRequest> onPlayerJoin(PlayerJoinEvent event);
+
+    ArrayList<PacketSendRequest> onClick(ClickEvent event);
 }
